95ee4b59bbf1116f36a8bd3f6ad8a00b111a20ad
Ibusann <user@USERnoMacBook-Pro.local>
2025-07-23 15:25:43 +0900

    講義23までの課題と機能を実装。 (1)StudentRepositoryにてMySQLのstudentsとstudents_courseからそれぞれデータを全件呼び出す。　 (2)mainにてユーザーのリクエスト（/student,/course）によってそれぞれのクラスに呼び出した要素を格納し、その結果を表示する。（全件検索機能の実装）

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -13,7 +13,6 @@ public class StudentManagementApplication {
 
 	@Autowired
 	private StudentRepository studentRepository;
-	private CourseRepository courseRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
