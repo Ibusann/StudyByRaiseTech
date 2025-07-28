4bc47226f4d9b9adc0ec2354e059b6d9e5c75262
Ibusann <user@USERnoMacBook-Pro.local>
2025-07-28 15:41:11 +0900

    講義25までの課題と機能を実装。

---
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -0,0 +1,30 @@
+package raisetech.StudentManagement.controller.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+import raisetech.StudentManagement.data.Course;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.domain.StudentDetail;
+
+@Component
+public class StudentConverter {
+
+  public List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<Course> courses) {
+    List<StudentDetail> studentDtails = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+
+      List<Course> convertCourses = courses.stream()
+          .filter(course -> student.getStudentId().equals(course.getStudentId()))
+          .collect(Collectors.toList());
+
+      studentDetail.setStudentCourse(convertCourses);
+      studentDtails.add(studentDetail);
+    });
+    return studentDtails;
+  }
+}
