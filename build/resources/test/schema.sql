1f51482c52b2696c28b2b7f19f1638d75d6914bb
Ibusann <user@USERnoMacBook-Pro.local>
2025-09-27 01:18:29 +0900

    dataとschemaをプッシュ -受講生の申し込み状況の情報を格納するstudents_courses_statusという新しいテーブルをMySQLに追加。 -受講生申込状況一覧を返すクラスを作成。 -申込状況の状態を条件に受講生もコース情報を検索する機能を追加。

--- build/resources/test/schema.sql
+++ build/resources/test/schema.sql
@@ -1,6 +1,8 @@
+
+
 CREATE TABLE IF NOT EXISTS students (
   student_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
+  name VARCHAR(100),
   kana_name VARCHAR(100),
   nickname VARCHAR(100),
   mail VARCHAR(100),
@@ -11,19 +13,22 @@ CREATE TABLE IF NOT EXISTS students (
   is_deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS students_courses (
-  course_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS students_courses
+(
+  course_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   student_id INT NOT NULL,
-  course VARCHAR(100),
-  start_course DATETIME,
-  end_course DATETIME
+  course varchar(100) DEFAULT NULL,
+  start_course datetime DEFAULT NULL,
+  end_course datetime DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS students_courses_status (
     status_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     student_id INT NOT NULL,
     course_id INT NOT NULL,
-    application_status VARCHAR(100) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    application_status ENUM('仮申込', '本申込', '受講中', '受講終了') NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES students_courses(course_id)
 );
